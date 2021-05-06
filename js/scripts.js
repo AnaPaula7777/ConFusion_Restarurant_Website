b76e920ab581deaebdda58db4db6c4e3ac52fb85
Ana Ramon <apramong@uwaterloo.ca>
2021-05-06 00:18:10 -0400

    NPM scripts Part I

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+    }});
+    $("#reserveTable").click(function(){
+        $("#reserveModal").modal("toggle");
+    });
+    $("#xReserveBtn").click(function(){
+        $("#reserveModal").modal("hide");
+    });
+    $("#cancelReserveBtn").click(function(){
+        $("#reserveModal").modal("hide");
+    });
+    $("#loginBtn").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+    $("#xLoginBtn").click(function(){
+        $("#loginModal").modal("hide");
+    });
+    $("#cancelLoginBtn").click(function(){
+        $("#loginModal").modal("hide");
+    });
+})
